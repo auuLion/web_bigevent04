b74d7a7dc90be056607b48cc1cfd022ff691f06c
auuLion <511944907@qq.com>
2020-11-27 10:55:33 +0800

    完成后台主页功能

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    initUserInfo()
+    //退出功能
+    $('#btnLogout').on('click', function () {
+        layer.confirm('是否要提出？', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+})
+//获取layui变量声明
+var layer = layui.layer
+//获取用户信息
+function initUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var text = name[0].toUpperCase()
+        $('.text-avatar').html(text).show()
+    }
+}
--- assets/js/webAPI.js
+++ assets/js/webAPI.js
@@ -2,5 +2,20 @@
 $(function () {
     $.ajaxPrefilter(function (options) {
         options.url = "http://ajax.frontend.itheima.net" + options.url
+        //获取需要权限的路径配置相应的请求头
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+        //对身份进行认证
+        options.complete = function (res) {
+            console.log(res);
+            if (res.responseJSON.status == 0 && res.responseJSON.message == '获取用户基本信息失败！') {
+                location.href = '/login.html'
+                localStorage.removeItem('token')
+            }
+        }
     })
+
 })
